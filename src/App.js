08eeb8a4910b8545dc03edb45a8b2a7eeedb2ad8
oghenenyerhovwo <93879454+oghenenyerhovwo@users.noreply.github.com>
2022-01-26 15:10:39 +0100

    Done with Drum pad layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,51 @@
-
+import {useState}from "react"
+import {Row, Col} from "react-bootstrap"
 
 
 function App() {
   return (
-    <div>
-      drum
+    <div className="drum-machine">
+
+        <Row className="display">
+            <Col lg={6} className="">
+              <div className="drum-pad">
+                  Q
+              </div>
+              <div className="drum-pad">
+                  W
+              </div>
+              <div className="drum-pad">
+                  E
+              </div> <br/>
+              <div className="drum-pad">
+                  A   
+              </div>
+              <div className="drum-pad">
+                  S
+              </div>
+              <div className="drum-pad">
+                  D
+              </div><br/>
+              <div className="drum-pad">
+                  Z
+              </div>
+              <div className="drum-pad">
+                  X
+              </div>
+              <div className="drum-pad">
+                  C
+              </div>
+            </Col>
+            <Col>
+                <div className="power">
+                  <h4>Power</h4>
+                  <div></div>
+                </div>
+                <div></div>
+                <div></div>
+                <div></div>
+            </Col>
+        </Row>
     </div>
   );
 }
