7fd95006ec99991dcd510f8bb16da3a01ee18102
oghenenyerhovwo <93879454+oghenenyerhovwo@users.noreply.github.com>
2022-01-27 20:47:46 +0100

    Styled all components

--- src/App.js
+++ src/App.js
@@ -1,53 +1,101 @@
 import {useState}from "react"
-import {Row, Col} from "react-bootstrap"
+import {Col, Row, } from "react-bootstrap"
 
 
 function App() {
-  return (
-    <div className="drum-machine">
+    const [togglePower, setTogglePower] = useState(true) 
+    const [toggleBank, setToggleBank] = useState(false) 
+    const [volume, setVolume] = useState(10)
+    const [screenText, setScreenText] = useState()
 
-        <Row className="display">
-            <Col lg={6} className="">
-              <div className="drum-pad">
-                  Q
-              </div>
-              <div className="drum-pad">
-                  W
-              </div>
-              <div className="drum-pad">
-                  E
-              </div> <br/>
-              <div className="drum-pad">
-                  A   
-              </div>
-              <div className="drum-pad">
-                  S
-              </div>
-              <div className="drum-pad">
-                  D
-              </div><br/>
-              <div className="drum-pad">
-                  Z
-              </div>
-              <div className="drum-pad">
-                  X
-              </div>
-              <div className="drum-pad">
-                  C
-              </div>
-            </Col>
-            <Col>
-                <div className="power">
-                  <h4>Power</h4>
-                  <div></div>
+    const handleVolume = num => {
+        setVolume(num)
+        setScreenText(`Volume ${num} `)
+    }
+
+    return (
+        <div className="drum-machine">
+            <div className="display">
+                <div>
+                <div className="drum-pad">
+                    Q
                 </div>
-                <div></div>
-                <div></div>
-                <div></div>
-            </Col>
-        </Row>
-    </div>
-  );
+                <div className="drum-pad">
+                    W
+                </div>
+                <div className="drum-pad">
+                    E
+                </div> <br/>
+                <div className="drum-pad">
+                    A   
+                </div>
+                <div className="drum-pad">
+                    S
+                </div>
+                <div className="drum-pad">
+                    D
+                </div><br/>
+                <div className="drum-pad">
+                    Z
+                </div>
+                <div className="drum-pad">
+                    X
+                </div>
+                <div className="drum-pad">
+                    C
+                </div>
+                </div>
+                <br/>
+                <br/>
+                <Row>
+                    <Col xs={12} sm={3}>
+                        <div className="power">
+                            <h6>Power</h6>
+                            <div className="bar">
+                                <div style={{opacity: togglePower ? 0: 1}} onClick={() => setTogglePower(prevToggle => !prevToggle)}></div>
+                                <div style={{opacity: !togglePower ? 0: 1}} onClick={() => setTogglePower(prevToggle => !prevToggle)}></div>
+                            </div>
+                        </div>
+                    </Col> 
+                    
+                    <Col xs={12} sm={6}>
+                        <div className="screen">{screenText} </div>
+                    </Col>
+                    <Col xs={12} sm={3}>
+                        <div className="bank">
+                            <h6>Bank</h6>
+                            <div className="bar">
+                                <div style={{opacity: toggleBank ? 0: 1}} onClick={() => setToggleBank(prevToggle => !prevToggle)}></div>
+                                <div style={{opacity: !toggleBank ? 0: 1}} onClick={() => setToggleBank(prevToggle => !prevToggle)}></div>
+                            </div>
+                        </div>
+                    </Col> 
+                </Row>
+                <div className="volume">
+                    <div onClick={() => handleVolume(1) } style={{background: volume === 1 ? "blue" : "none" }} className="bar">{volume !== 1 && <div></div>}</div>
+                    <div onClick={() => handleVolume(2) } style={{background: volume === 2 ? "blue" : "none" }} className="bar">{volume !== 2 && <div></div>}</div>
+                    <div onClick={() => handleVolume(3) } style={{background: volume === 3 ? "blue" : "none" }} className="bar">{volume !== 3 && <div></div>}</div>
+                    <div onClick={() => handleVolume(4) } style={{background: volume === 4 ? "blue" : "none" }} className="bar">{volume !== 4 && <div></div>}</div>
+                    <div onClick={() => handleVolume(5) } style={{background: volume === 5 ? "blue" : "none" }} className="bar">{volume !== 5 && <div></div>}</div>
+                    <div onClick={() => handleVolume(6) } style={{background: volume === 6 ? "blue" : "none" }} className="bar">{volume !== 6 && <div></div>}</div>
+                    <div onClick={() => handleVolume(7) } style={{background: volume === 7 ? "blue" : "none" }} className="bar">{volume !== 7 && <div></div>}</div>
+                    <div onClick={() => handleVolume(8) } style={{background: volume === 8 ? "blue" : "none" }} className="bar">{volume !== 8 && <div></div>}</div>
+                    <div onClick={() => handleVolume(9) } style={{background: volume === 9 ? "blue" : "none" }} className="bar">{volume !== 9 && <div></div>}</div>
+                    <div onClick={() => handleVolume(10) } style={{background: volume === 10 ? "blue" : "none" }} className="bar">{volume !== 10 && <div></div>}</div>
+                    <div onClick={() => handleVolume(11) } style={{background: volume === 11 ? "blue" : "none" }} className="bar">{volume !== 11 && <div></div>}</div>
+                    <div onClick={() => handleVolume(12) } style={{background: volume === 12 ? "blue" : "none" }} className="bar">{volume !== 12 && <div></div>}</div>
+                    <div onClick={() => handleVolume(13) } style={{background: volume === 13 ? "blue" : "none" }} className="bar">{volume !== 13 && <div></div>}</div>
+                    <div onClick={() => handleVolume(14) } style={{background: volume === 14 ? "blue" : "none" }} className="bar">{volume !== 14 && <div></div>}</div>
+                    <div onClick={() => handleVolume(15) } style={{background: volume === 15 ? "blue" : "none" }} className="bar">{volume !== 15 && <div></div>}</div>
+                    <div onClick={() => handleVolume(16) } style={{background: volume === 16 ? "blue" : "none" }} className="bar">{volume !== 16 && <div></div>}</div>
+                    <div onClick={() => handleVolume(17) } style={{background: volume === 17 ? "blue" : "none" }} className="bar">{volume !== 17 && <div></div>}</div>
+                    <div onClick={() => handleVolume(18) } style={{background: volume === 18 ? "blue" : "none" }} className="bar">{volume !== 18 && <div></div>}</div>
+                    <div onClick={() => handleVolume(19) } style={{background: volume === 19 ? "blue" : "none" }} className="bar">{volume !== 19 && <div></div>}</div>
+                    <div onClick={() => handleVolume(20) } style={{background: volume === 20 ? "blue" : "none" }} className="bar">{volume !== 20 && <div></div>}</div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
