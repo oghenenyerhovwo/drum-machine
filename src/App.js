3243a406186a395d599191ea66c110dc806b9d1e
oghenenyerhovwo <93879454+oghenenyerhovwo@users.noreply.github.com>
2022-03-18 21:53:14 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,228 +1,117 @@
-import React, {useEffect, useState}from "react"
-import {Col, Row, } from "react-bootstrap"
-import {audio1,audio2} from "./audio_files"
+import React, {useEffect,useRef, useState}from "react"
 
 function App() {
-    const [audio, setAudio] = useState(audio1)
-    const [togglePower, setTogglePower] = useState(true) 
-    const [toggleBank, setToggleBank] = useState(false) 
-    const [volume, setVolume] = useState(10)
-    const [screenText, setScreenText] = useState()
-    const [toggleBackgrounds, setToggleBackgrounds] = useState({
-        q: false,
-        w: false,
-        e: false,
-        a: false,
-        s: false,
-        d: false,
-        z: false,
-        x: false,
-        c: false,
-    })
-    const myRefQ = React.createRef();
-    const myRefW = React.createRef();
-    const myRefE = React.createRef();
-    const myRefA = React.createRef();
-    const myRefS = React.createRef();
-    const myRefD = React.createRef();
-    const myRefZ = React.createRef();
-    const myRefX = React.createRef();
-    const myRefC = React.createRef();
+    const allCharacters = ["0","1", "2","3","4","5","6","7","8","9","+","-","/","*","(",")"]
+    const operators = ["/","*"]
+    const [text, setText] = useState("")
+    const [maths, setMaths] = useState("")
+    const [solution, setSolution] = useState("0")
+    const inputRef = useRef()
 
-    const handleKeyPress = (event) => {
-        if(togglePower){
-            if(event.key === "q"){
-                myRefQ.current.play()
-                myRefQ.current.volume = (volume/20)
-                setScreenText(myRefQ.current.attributes.name.value)
-                changeBackground(myRefQ.current.attributes.id.value.toLowerCase())
-            }
-            if(event.key === "w"){
-                myRefW.current.play()
-                myRefW.current.volume = (volume/20)
-                setScreenText(myRefW.current.attributes.name.value)
-                changeBackground(myRefW.current.attributes.id.value.toLowerCase())
-            }
-            if(event.key === "e"){
-                myRefE.current.play()
-                myRefE.current.volume = (volume/20)
-                setScreenText(myRefE.current.attributes.name.value)
-                changeBackground(myRefE.current.attributes.id.value.toLowerCase())
-            }
-            if(event.key === "a"){
-                myRefA.current.play()
-                myRefA.current.volume = (volume/20)
-                setScreenText(myRefA.current.attributes.name.value)
-                changeBackground(myRefA.current.attributes.id.value.toLowerCase())
-            }
-            if(event.key === "s"){
-                myRefS.current.play()
-                myRefS.current.volume = (volume/20)
-                setScreenText(myRefS.current.attributes.name.value)
-                changeBackground(myRefS.current.attributes.id.value.toLowerCase())
-            }
-            if(event.key === "d"){
-                myRefD.current.play()
-                myRefD.current.volume = (volume/20)
-                setScreenText(myRefD.current.attributes.name.value)
-                changeBackground(myRefD.current.attributes.id.value.toLowerCase())
-            }
-            if(event.key === "z"){
-                myRefZ.current.play()
-                myRefZ.current.volume = (volume/20)
-                setScreenText(myRefZ.current.attributes.name.value)
-                changeBackground(myRefZ.current.attributes.id.value.toLowerCase())
-            }
-            if(event.key === "x"){
-                myRefX.current.play()
-                myRefX.current.volume = (volume/20)
-                setScreenText(myRefX.current.attributes.name.value)
-                changeBackground(myRefX.current.attributes.id.value.toLowerCase())
-            }
-            if(event.key === "c"){
-                myRefC.current.play()
-                myRefC.current.volume = (volume/20)
-                setScreenText(myRefC.current.attributes.name.value)
-                changeBackground(myRefC.current.attributes.id.value.toLowerCase())
-            }
+    const handleChange = e =>{
+        setText(e.target.value)
+    }
+
+    const solve = (exp) => {
+        try {
+            setSolution(eval(exp))
+        } catch (error) {
+            setSolution("...")
         }
-    };
     
+    }
 
     useEffect(() => {
-        document.addEventListener("keydown", handleKeyPress);
-    })
+        // checking if the last character type is valid
+        if(maths.length < text.length){
+            const lastChar = text[text.length-1]
+        
+            if(allCharacters.includes(lastChar)){
+                if(!(text.length === 1 && operators.includes(lastChar))){
+                    setMaths(text)
+                    solve(text)
+                } else{
+                    setText(text.slice(0,-1))
+                }
+                
+            } else {
+                setText(text.slice(0,-1))
+            }
+        } else{
+            setMaths(text)
+            solve(text)
+        }
+    }, [text])
 
     useEffect(() => {
-      if(!toggleBank){
-        setAudio(audio1)
-      } else {
-        setAudio(audio2)
-      }
-    }, [toggleBank])
-
-    const handleVolume = num => {
-        setVolume(num)
-        setScreenText(`Volume ${num} `)
-    }
-
-    const handlePadClick = (name, ref,e) => {
-        if(togglePower){
-            ref.current.play();
-            ref.current.volume = (volume/20)
-            setScreenText(name)
-            changeBackground(e.target.children[0].id.toLowerCase())
+        if(inputRef.current){
+                setTimeout(() => {
+                   inputRef.current.focus() 
+                }); 
+            
         }
-    }
+    }, [text])
 
-    const changeBackground = (letter) => {
-        setToggleBackgrounds({
-            ...toggleBackgrounds,
-            [letter]: true
-        })
-        setTimeout(() => {
-            setToggleBackgrounds({
-                ...toggleBackgrounds,
-                [letter]: false,
-            })
-        }, 200);
-    }
-
-    const handleToggle = text => {
-        if(togglePower){
-            setToggleBank(prevToggle => !prevToggle)
-            setScreenText(text)
+    useEffect(() => {
+        if(!text){
+            setSolution("0")
         }
+    }, [text])
+
+
+
+    const reset = () => {
+        setText("")
+    }
+    
+    const removeChar = () => {
+        setText(text.slice(0,-1))
+    }
+    
+    const equalTo = () => {
+        solve(maths)
+        navigator.clipboard.writeText(solution)
     }
 
+    const handleKeyClick = e => {
+        setText(prevText => prevText + e.target.innerHTML)
+    }
+    
     return (
-        <div className="drum-machine">
-            <div className="display">
-                <div>
-                <div style={{background: !toggleBackgrounds.q ? "rgb(44, 56, 56)" : "rgb(255, 215, 0)" }} onClick={(e) => handlePadClick(audio.q.name,myRefQ,e)} className="drum-pad">
-                    <audio id="Q" name={audio.q.name} type="audio/mpeg" ref={myRefQ} className="clip" src={audio.q.soundUrl} > </audio>
-                    Q
+        <div className="outer-div">
+            <div className="calc">
+                <input ref={inputRef} id="display" onChange={handleChange} value={maths} type="string"/>
+                <div className="answer">{solution} </div>
+                <br/><br/>
+                <div className="row first-row">
+                    <div onClick={reset} className="keys reset">C</div>
+                    <div onClick={handleKeyClick} className="num-keys keys border">/</div>
+                    <div onClick={handleKeyClick} className="num-keys keys border">*</div>
+                    <div onClick={removeChar} className="keys border delete">{"<"} </div>
                 </div>
-                <div style={{background: !toggleBackgrounds.w ? "rgb(44, 56, 56)" : "rgb(255, 215, 0)" }} onClick={(e) => handlePadClick(audio.w.name,myRefW,e)} className="drum-pad">
-                    <audio id="W" name={audio.w.name} type="audio/mpeg" ref={myRefW} className="clip" src={audio.w.soundUrl} > </audio>
-                    W
+                <div className="row other-row">
+                    <div onClick={handleKeyClick} className="num-keys keys">7</div>
+                    <div onClick={handleKeyClick} className="num-keys keys border">8</div>
+                    <div onClick={handleKeyClick} className="num-keys keys border">9</div>
+                    <div onClick={handleKeyClick} className="num-keys keys border">-</div>
                 </div>
-                <div style={{background: !toggleBackgrounds.e ? "rgb(44, 56, 56)" : "rgb(255, 215, 0)" }} onClick={(e) => handlePadClick(audio.e.name,myRefE,e)} className="drum-pad">
-                    <audio id="E" name={audio.e.name} type="audio/mpeg" ref={myRefE} className="clip" src={audio.e.soundUrl} > </audio>
-                    E
-                </div> <br/>
-                <div style={{background: !toggleBackgrounds.a ? "rgb(44, 56, 56)" : "rgb(255, 215, 0)" }} onClick={(e) => handlePadClick(audio.a.name,myRefA,e)} className="drum-pad">
-                    <audio id="A" name={audio.a.name} type="audio/mpeg" ref={myRefA} className="clip" src={audio.a.soundUrl} > </audio>
-                    A   
+                <div className="row other-row">
+                    <div onClick={handleKeyClick} className="num-keys keys">4</div>
+                    <div onClick={handleKeyClick} className="num-keys keys border">5</div>
+                    <div onClick={handleKeyClick} className="num-keys keys border">6</div>
+                    <div onClick={handleKeyClick} className="num-keys keys border">+</div>
                 </div>
-                <div style={{background: !toggleBackgrounds.s ? "rgb(44, 56, 56)" : "rgb(255, 215, 0)" }} onClick={(e) => handlePadClick(audio.s.name,myRefS,e)} className="drum-pad">
-                    <audio id="S" name={audio.s.name} type="audio/mpeg" ref={myRefS} className="clip" src={audio.s.soundUrl} > </audio>
-                    S
+                <div className="row other-row">
+                    <div onClick={handleKeyClick} className="num-keys keys">1</div>
+                    <div onClick={handleKeyClick} className="num-keys keys border">2</div>
+                    <div onClick={handleKeyClick} className="num-keys keys border">3</div>
+                    <div onClick={handleKeyClick} className="num-keys keys border">.</div>
                 </div>
-                <div style={{background: !toggleBackgrounds.d ? "rgb(44, 56, 56)" : "rgb(255, 215, 0)" }} onClick={(e) => handlePadClick(audio.d.name,myRefD,e)} className="drum-pad">
-                    <audio id="D" name={audio.d.name} type="audio/mpeg" ref={myRefD} className="clip" src={audio.d.soundUrl} > </audio>
-                    D
-                </div><br/>
-                <div style={{background: !toggleBackgrounds.z ? "rgb(44, 56, 56)" : "rgb(255, 215, 0)" }} onClick={(e) => handlePadClick(audio.z.name,myRefZ,e)} className="drum-pad">
-                    <audio id="Z" name={audio.z.name} type="audio/mpeg" ref={myRefZ} className="clip" src={audio.z.soundUrl} > </audio>
-                    Z
-                </div>
-                <div style={{background: !toggleBackgrounds.x ? "rgb(44, 56, 56)" : "rgb(255, 215, 0)" }} onClick={(e) => handlePadClick(audio.x.name,myRefX,e)} className="drum-pad">
-                    <audio id="X" name={audio.x.name} type="audio/mpeg" ref={myRefX} className="clip" src={audio.x.soundUrl} > </audio>
-                    X
-                </div>
-                <div style={{background: !toggleBackgrounds.c ? "rgb(44, 56, 56)" : "rgb(255, 215, 0)" }} onClick={(e) => handlePadClick(audio.c.name,myRefC,e)} className="drum-pad">
-                    <audio id="C" name={audio.c.name} type="audio/mpeg" ref={myRefC} className="clip" src={audio.c.soundUrl} > </audio>
-                    C
-                </div>
-                </div>
-                <br/>
-                <br/>
-                <Row>
-                    <Col xs={12} sm={3}>
-                        <div className="power">
-                            <h6>Power</h6>
-                            <div className="bar">
-                                <div style={{opacity: togglePower ? 0: 1}} onClick={() => setTogglePower(prevToggle => !prevToggle)}></div>
-                                <div style={{opacity: !togglePower ? 0: 1}} onClick={() => setTogglePower(prevToggle => !prevToggle)}></div>
-                            </div>
-                        </div>
-                    </Col> 
-                    
-                    <Col xs={12} sm={6}>
-                        <div className="screen">{screenText} </div>
-                    </Col>
-                    <Col xs={12} sm={3}>
-                        <div className="bank">
-                            <h6>Bank</h6>
-                            <div className="bar">
-                                <div style={{opacity: toggleBank ? 0: 1}} onClick={() => handleToggle("Heater Kit")}></div>
-                                <div style={{opacity: !toggleBank ? 0: 1}} onClick={() => handleToggle("Smooth Piano Kit")}></div>
-                            </div>
-                        </div>
-                    </Col> 
-                </Row>
-                <div className="volume">
-                    <div onClick={() => handleVolume(1) } style={{background: volume === 1 ? "blue" : "none" }} className="bar">{volume !== 1 && <div></div>}</div>
-                    <div onClick={() => handleVolume(2) } style={{background: volume === 2 ? "blue" : "none" }} className="bar">{volume !== 2 && <div></div>}</div>
-                    <div onClick={() => handleVolume(3) } style={{background: volume === 3 ? "blue" : "none" }} className="bar">{volume !== 3 && <div></div>}</div>
-                    <div onClick={() => handleVolume(4) } style={{background: volume === 4 ? "blue" : "none" }} className="bar">{volume !== 4 && <div></div>}</div>
-                    <div onClick={() => handleVolume(5) } style={{background: volume === 5 ? "blue" : "none" }} className="bar">{volume !== 5 && <div></div>}</div>
-                    <div onClick={() => handleVolume(6) } style={{background: volume === 6 ? "blue" : "none" }} className="bar">{volume !== 6 && <div></div>}</div>
-                    <div onClick={() => handleVolume(7) } style={{background: volume === 7 ? "blue" : "none" }} className="bar">{volume !== 7 && <div></div>}</div>
-                    <div onClick={() => handleVolume(8) } style={{background: volume === 8 ? "blue" : "none" }} className="bar">{volume !== 8 && <div></div>}</div>
-                    <div onClick={() => handleVolume(9) } style={{background: volume === 9 ? "blue" : "none" }} className="bar">{volume !== 9 && <div></div>}</div>
-                    <div onClick={() => handleVolume(10) } style={{background: volume === 10 ? "blue" : "none" }} className="bar">{volume !== 10 && <div></div>}</div>
-                    <div onClick={() => handleVolume(11) } style={{background: volume === 11 ? "blue" : "none" }} className="bar">{volume !== 11 && <div></div>}</div>
-                    <div onClick={() => handleVolume(12) } style={{background: volume === 12 ? "blue" : "none" }} className="bar">{volume !== 12 && <div></div>}</div>
-                    <div onClick={() => handleVolume(13) } style={{background: volume === 13 ? "blue" : "none" }} className="bar">{volume !== 13 && <div></div>}</div>
-                    <div onClick={() => handleVolume(14) } style={{background: volume === 14 ? "blue" : "none" }} className="bar">{volume !== 14 && <div></div>}</div>
-                    <div onClick={() => handleVolume(15) } style={{background: volume === 15 ? "blue" : "none" }} className="bar">{volume !== 15 && <div></div>}</div>
-                    <div onClick={() => handleVolume(16) } style={{background: volume === 16 ? "blue" : "none" }} className="bar">{volume !== 16 && <div></div>}</div>
-                    <div onClick={() => handleVolume(17) } style={{background: volume === 17 ? "blue" : "none" }} className="bar">{volume !== 17 && <div></div>}</div>
-                    <div onClick={() => handleVolume(18) } style={{background: volume === 18 ? "blue" : "none" }} className="bar">{volume !== 18 && <div></div>}</div>
-                    <div onClick={() => handleVolume(19) } style={{background: volume === 19 ? "blue" : "none" }} className="bar">{volume !== 19 && <div></div>}</div>
-                    <div onClick={() => handleVolume(20) } style={{background: volume === 20 ? "blue" : "none" }} className="bar">{volume !== 20 && <div></div>}</div>
+                <div className="row other-row">
+                    <div onClick={handleKeyClick} className="num-keys keys">(</div>
+                    <div onClick={handleKeyClick} className="num-keys keys border">0</div>
+                    <div onClick={handleKeyClick} className="num-keys keys border">)</div>
+                    <div onClick={equalTo} className="keys border blue equal">=</div>
                 </div>
             </div>
         </div>
